639634da70a2a9c0ea1021dd39778bc433aa4827
RuslanKaysaks <39536896+RuslanKaysaks@users.noreply.github.com>
2019-03-10 00:01:40 +0300

    add

--- static/js/main.js
+++ static/js/main.js
@@ -76,6 +76,7 @@ $(document).ready(function () {
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
     sandwich();
     sliderReviews();
     sliderTabs();
