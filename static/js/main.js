475005be9fa16940afe120a8e57f2b32e40fcb44
RuslanKaysaks <39536896+RuslanKaysaks@users.noreply.github.com>
2019-04-05 15:48:20 +0300

    add

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,3 @@
 $(document).ready(function () {
     svg4everybody({});
-
-    let sliderRev = () => {
-    	$('.js-slider-reviews').slick({
-    		slidesToShow: 3,
-    		slidesToScoll: 1,
-    		prevArrow: '.navigation--prev',
-    		nextArrow: '.navigation--next',
-    		responsive: [
-			    {
-			      breakpoint: 800,
-			      settings: {
-			        slidesToShow: 2,
-			        slidesToScroll: 2
-			      }
-			    },
-			    {
-			      breakpoint: 600,
-			      settings: {
-			        slidesToShow: 1,
-			        slidesToScroll: 1
-			      }
-			    }
-			  ]
-    	});
-    }
-    sliderRev();
-    
 });
