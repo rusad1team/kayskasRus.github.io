89a6e79f9349666f7d65d37836b59adedf7cf84f
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-02-01 04:56:44 +0300

    nnew

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
 
 svg4everybody({});
+$('.tel-icon').on('click', function(){
+	$('.tel-link').slideToggle();
+	$('.mail-link').css('display', 'none');
+});
+$('.mail-icon').on('click', function(){
+	$('.mail-link').slideToggle();
+	$('.tel-link').css('display', 'none');
+});
+$('.mod-titleBox').on('click', function(){
+	$(this).toggleClass('in').next().slideToggle();
+
+	$('.mod-titleBox').not(this).removeClass('in').next().slideUp();
+});
 });
