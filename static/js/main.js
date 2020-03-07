1737b28cb932192bc7803254bf3642bd6d32d091
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-03-07 22:51:07 +0300

    zamena

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,39 @@
 $(document).ready(function () {
+//скролл
+$('.btnScroll').click(function (){
+var elementClick = $(this).attr("href");
+var destination = $(elementClick).offset().top;
+jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+return false;
+});
 
-svg4everybody({});
-$('.tel-icon').on('click', function(){
-	$('.tel-link').slideToggle();
-	$('.mail-link').css('display', 'none');
-});
-$('.mail-icon').on('click', function(){
-	$('.mail-link').slideToggle();
-	$('.tel-link').css('display', 'none');
-});
-$('.mod-titleBox').on('click', function(){
-	$(this).toggleClass('in').next().slideToggle();
+let tab = function () {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName;
 
-	$('.mod-titleBox').not(this).removeClass('in').next().slideUp();
+    tabNav.forEach( item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach( item => {
+            item.classList.remove('is-active');
+        });
+        this.classList.add('is-active');
+
+        tabName = this.getAttribute('data-tab-name');
+
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName)? item.classList.add('is-active'): item.classList.remove('is-active');
+        })
+    }
+};
+
+tab();
 });
-});
+
