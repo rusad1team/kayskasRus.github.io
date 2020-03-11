1e566ba0900a016805139851a6016ff4e5bb4db8
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-03-11 11:05:09 +0300

    addd

--- static/js/main.js
+++ static/js/main.js
@@ -1,163 +1,162 @@
 $(document).ready(function () {
 //скролл
-$('.linkScroll').click(function (){
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top;
-        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
-        return false;
-    })
-
-let tabSlider = function () {
-    $('.tabs-nav').slick({
-    mobileFirst: true,
-    arrows: false,
-    dots: true,
-    appendDots: '.navig-dots2',
-    customPaging: function(slider,i) {
-        return '<div class="navig-dot2"></div>';
-},
-    responsive: [
-        {
-            breakpoint: 801,
-            settings: "unslick",
-            slidesToShow: 3,
-            arrows: false,
-        },
-         {
-            breakpoint: 400,
-            slidesToShow: 1,
-            arrows: false,
-        }
-    ]
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('.toTop').fadeIn();
+ 
+} else {
+ 
+$('.toTop').fadeOut();
+ 
+}
+ 
 });
+ 
+$('.toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
+
+$('.cart__maps').each(function(){
+        ymaps.ready(function (){
+            var coords = $('#map').data('coords');
+            var coords_up = '[' + coords + ']';
+            coords_json = JSON.parse(coords_up);
+            var myMap = new ymaps.Map('map', {
+                    center: coords_json,
+                    zoom: 10,
+                    controls: ['geolocationControl','zoomControl']
+                }, {
+                    searchControlProvider: 'yandex#search'
+                }),
+
+                myPlacemarkWithContent = new ymaps.Placemark(coords_json, {
+                    hintContent: 'Мы на карте',
+                }, {iconLayout: 'default#image',iconImageHref: '/static/images/general/balun.png',iconImageSize: [71, 90],iconImageOffset: [-35, -90]});
+
+                myMap.geoObjects
+                .add(myPlacemarkWithContent);
+        });
+    });
+$('.nav-link').click(function (){
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
+});
+//stinky header
+
+if ($(window).width() > 800) {
+   window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("nav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+}
+//sandwicj
+let sanwichToggle = function() {
+    $('.sandwich').click(function(){
+       $('.sandwich-line').toggleClass('sandwich--active');
+       $('.nav-list').fadeToggle('slow'); 
+    });
+};
+sanwichToggle();
+
+});
+
+//таймер
+function getDayEnd() {
+    var dayEnd = new Date();
+    dayEnd.setHours(35, 59, 59);
+
+    return dayEnd;
+}
+if (window.dayEndTimestamp === undefined) {
+    window.dayEndTimestamp = getDayEnd();
+}
+function getTimeRemaining(endtime) {
+    var t       = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours   = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+    return {
+        'total':   t,
+        'hours':   hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
 }
 
-$('.image-modal').each(function(){
-        $(this).magnificPopup({
-            type:'inline',
-            fixedContentPos: true,
-            removalDelay: 100,
-            closeBtnInside: true,
-            preloader: true,
-            mainClass: 'mfp-fade',
-            callbacks: {
-                open: function () {
-                    $('.slider-portfolio').not('.slick-initialized').slick('refresh');
-                    $('html').css('margin-right', '5px');
-                }
-                // e.t.c.
-            }
-        });
-    });
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        '(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'
+    ));
 
-let tab = function () {
-    let tabNav = document.querySelectorAll('.tabs-nav__item'),
-        tabContent = document.querySelectorAll('.tab'),
-        tabName;
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
-    tabNav.forEach( item => {
-        item.addEventListener('click', selectTabNav)
-    });
+function initializeClock(id, endtime) {
+    var clock       = document.getElementById(id),
+        hoursSpan   = clock.querySelector('.hours'),
+        minutesSpan = clock.querySelector('.minutes'),
+        secondsSpan = clock.querySelector('.seconds');
 
-    function selectTabNav() {
-        tabNav.forEach( item => {
-            item.classList.remove('is-active');
-        });
-        this.classList.add('is-active');
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
 
-        tabName = this.getAttribute('data-tab-name');
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-        selectTabContent(tabName);
+        if (t.total <= 0) {
+            hoursSpan.innerHTML = ('00');
+            minutesSpan.innerHTML = ('00');
+            secondsSpan.innerHTML = ('00');
+            clearInterval(timeinterval);
+        }
     }
 
-    function selectTabContent(tabName) {
-        tabContent.forEach(item => {
-            item.classList.contains(tabName)? item.classList.add('is-active'): item.classList.remove('is-active');
-        })
-    }
-};
+    updateClock();
 
-// mask
-        $(".mask").mask("+7 (999) 999-99-99");
-    
-    
+    var timeinterval = setInterval(updateClock, 500);
+}
 
-    // показать скрыть popup
-        $('body').on('click', '.popup-view', function(){
-            $('.callme-popup .form-hide .popup-form__title').text($(this).data('title'));
-            $('.callme-popup .form-hide .popup-form__desc').text($(this).data('desc'));
-            if ($(this).data('file') == 'Да'){
-                $('.callme-popup .form-hide .popup-form__file').show();
-            } else {
-                $('.callme-popup .form-hide .popup-form__file').hide();
-            }
-            $('.callme-popup .form-metka').val($(this).data('metka'));
-            $('.callme-popup .form-button').text($(this).data('button'));
-            $('.callme-popup').fadeIn(300);
-                $('.popup-form').animate({
-                opacity: 1
-            }, 300, 'linear');
-            var metrika = $('btn-' + $(this).data('id'));
-            //yaCounter47342694.reachGoal(metrika);
-            //dataLayer.push({'event': 'order_site'});
-            return false;
-        });
-        $('.callme-popup').on('click', '.popup-close, .popup-close--2', function(){
-            $('.popup-form').animate({
-            opacity: 0
-        }, 300, 'linear');
-            setTimeout(function(){
-               $('.callme-popup').fadeOut(300);
-            },200);
-        });
-    // форма заявки общая
-    $('.form-js').each(function(){
-        var cur_form = '#' + $(this).attr('id');
-        var cur_form_file = $(this).attr('id');
-        $(this).on('click', '.form-button', function(){
-            if ($(cur_form + ' .validate').val() == ''){
-                $(cur_form + ' .validate').css({'border-color':'#de2827'});
-                setTimeout(function(){
-                    $('.validate').removeAttr('style');
-                },500);
-                return false;
-            } else {
-                if ($(cur_form + ' .confidencial__checked input').prop('checked')){
-                    $(cur_form).closest('.form-hide').hide();
-                    $(cur_form).closest('.form-hide').siblings('.form-ok').fadeIn(100);
-                    setTimeout(function(){
-                        var ev = new Event("submit");
-                        document.getElementById(cur_form_file).dispatchEvent(ev);
-                    }, 500);
-                    document.getElementById(cur_form_file).addEventListener('submit', function(evt){
-                        var http = new XMLHttpRequest(), f = this;
-                        evt.preventDefault();
-                        http.open("POST", "/js/contacts.php", true);
-                        http.onerror = function(){
-                            console.log("Данные не были переданы");
-                        }
-                        http.send(new FormData(f)); 
-                    }, false);
-                    console.log("Отправили");
-                    //yaCounter47342694.reachGoal('popup_form');
-                    //dataLayer.push({'event': 'order_site'});
-                } else {
-                    $(cur_form + ' .confidencial__checked').css('color','#de2827');
-                    setTimeout(function(){
-                        $('.confidencial__checked').removeAttr('style');
-                    },500);
-                    return false;
-                };
-            }
-        });
-    });
+var myClock = (document.cookie && document.cookie.match('myClock')) ?
+                (Date.parse(getCookie('myClock')) - Date.now() > -60000) : null;
+
+// if there's a cookie with the name myClock, and timer stoped less then 1 minute (-60000) ago, use that value as the deadline
+if (myClock) {
+    // get deadline value from cookie
+    var deadline = getCookie('myClock');
+}
+// otherwise, set a deadline 45 minutes from now and
+// save it in a cookie with that name
+else {
+    // create deadline 45 minutes from now
+    var timeInMinutes = 45,
+        currentTime   = Date.parse(new Date().toString()),
+        deadline      = new Date(currentTime + timeInMinutes * 60 * 1000);
+
+    // store deadline in cookie for future reference
+    document.cookie = 'myClock=' + deadline;
+}
+
+initializeClock('timer', window.dayEndTimestamp);
 
 
 
-tab();
-tabSlider();
-});
-
-
-//mask
-!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):a("object"==typeof exports?require("jquery"):jQuery)}(function(a){var b,c=navigator.userAgent,d=/iphone/i.test(c),e=/chrome/i.test(c),f=/android/i.test(c);a.mask={definitions:{9:"[0-9]",a:"[A-Za-z]","*":"[A-Za-z0-9]"},autoclear:!0,dataName:"rawMaskFn",placeholder:"_"},a.fn.extend({caret:function(a,b){var c;if(0!==this.length&&!this.is(":hidden"))return"number"==typeof a?(b="number"==typeof b?b:a,this.each(function(){this.setSelectionRange?this.setSelectionRange(a,b):this.createTextRange&&(c=this.createTextRange(),c.collapse(!0),c.moveEnd("character",b),c.moveStart("character",a),c.select())})):(this[0].setSelectionRange?(a=this[0].selectionStart,b=this[0].selectionEnd):document.selection&&document.selection.createRange&&(c=document.selection.createRange(),a=0-c.duplicate().moveStart("character",-1e5),b=a+c.text.length),{begin:a,end:b})},unmask:function(){return this.trigger("unmask")},mask:function(c,g){var h,i,j,k,l,m,n,o;if(!c&&this.length>0){h=a(this[0]);var p=h.data(a.mask.dataName);return p?p():void 0}return g=a.extend({autoclear:a.mask.autoclear,placeholder:a.mask.placeholder,completed:null},g),i=a.mask.definitions,j=[],k=n=c.length,l=null,a.each(c.split(""),function(a,b){"?"==b?(n--,k=a):i[b]?(j.push(new RegExp(i[b])),null===l&&(l=j.length-1),k>a&&(m=j.length-1)):j.push(null)}),this.trigger("unmask").each(function(){function h(){if(g.completed){for(var a=l;m>=a;a++)if(j[a]&&C[a]===p(a))return;g.completed.call(B)}}function p(a){return g.placeholder.charAt(a<g.placeholder.length?a:0)}function q(a){for(;++a<n&&!j[a];);return a}function r(a){for(;--a>=0&&!j[a];);return a}function s(a,b){var c,d;if(!(0>a)){for(c=a,d=q(b);n>c;c++)if(j[c]){if(!(n>d&&j[c].test(C[d])))break;C[c]=C[d],C[d]=p(d),d=q(d)}z(),B.caret(Math.max(l,a))}}function t(a){var b,c,d,e;for(b=a,c=p(a);n>b;b++)if(j[b]){if(d=q(b),e=C[b],C[b]=c,!(n>d&&j[d].test(e)))break;c=e}}function u(){var a=B.val(),b=B.caret();if(o&&o.length&&o.length>a.length){for(A(!0);b.begin>0&&!j[b.begin-1];)b.begin--;if(0===b.begin)for(;b.begin<l&&!j[b.begin];)b.begin++;B.caret(b.begin,b.begin)}else{for(A(!0);b.begin<n&&!j[b.begin];)b.begin++;B.caret(b.begin,b.begin)}h()}function v(){A(),B.val()!=E&&B.change()}function w(a){if(!B.prop("readonly")){var b,c,e,f=a.which||a.keyCode;o=B.val(),8===f||46===f||d&&127===f?(b=B.caret(),c=b.begin,e=b.end,e-c===0&&(c=46!==f?r(c):e=q(c-1),e=46===f?q(e):e),y(c,e),s(c,e-1),a.preventDefault()):13===f?v.call(this,a):27===f&&(B.val(E),B.caret(0,A()),a.preventDefault())}}function x(b){if(!B.prop("readonly")){var c,d,e,g=b.which||b.keyCode,i=B.caret();if(!(b.ctrlKey||b.altKey||b.metaKey||32>g)&&g&&13!==g){if(i.end-i.begin!==0&&(y(i.begin,i.end),s(i.begin,i.end-1)),c=q(i.begin-1),n>c&&(d=String.fromCharCode(g),j[c].test(d))){if(t(c),C[c]=d,z(),e=q(c),f){var k=function(){a.proxy(a.fn.caret,B,e)()};setTimeout(k,0)}else B.caret(e);i.begin<=m&&h()}b.preventDefault()}}}function y(a,b){var c;for(c=a;b>c&&n>c;c++)j[c]&&(C[c]=p(c))}function z(){B.val(C.join(""))}function A(a){var b,c,d,e=B.val(),f=-1;for(b=0,d=0;n>b;b++)if(j[b]){for(C[b]=p(b);d++<e.length;)if(c=e.charAt(d-1),j[b].test(c)){C[b]=c,f=b;break}if(d>e.length){y(b+1,n);break}}else C[b]===e.charAt(d)&&d++,k>b&&(f=b);return a?z():k>f+1?g.autoclear||C.join("")===D?(B.val()&&B.val(""),y(0,n)):z():(z(),B.val(B.val().substring(0,f+1))),k?b:l}var B=a(this),C=a.map(c.split(""),function(a,b){return"?"!=a?i[a]?p(b):a:void 0}),D=C.join(""),E=B.val();B.data(a.mask.dataName,function(){return a.map(C,function(a,b){return j[b]&&a!=p(b)?a:null}).join("")}),B.one("unmask",function(){B.off(".mask").removeData(a.mask.dataName)}).on("focus.mask",function(){if(!B.prop("readonly")){clearTimeout(b);var a;E=B.val(),a=A(),b=setTimeout(function(){B.get(0)===document.activeElement&&(z(),a==c.replace("?","").length?B.caret(0,a):B.caret(a))},10)}}).on("blur.mask",v).on("keydown.mask",w).on("keypress.mask",x).on("input.mask paste.mask",function(){B.prop("readonly")||setTimeout(function(){var a=A(!0);B.caret(a),h()},0)}),e&&f&&B.off("input.mask").on("input.mask",u),A()})}})});
