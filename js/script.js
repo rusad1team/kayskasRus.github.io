65adfb2e98ecf8dbfc66ec2663cf088b1f29075e
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-04-25 05:42:35 +0300

    adddn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,174 @@
-$(document).ready(function(){$(".linkScroll").click(function(){var a=$(this).attr("href");a=$(a).offset().top;jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop:a},800);return!1});(function(){$(".slider-for").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,lazyLoad:"ondemand",fade:!0,dots:!0,asNavFor:".slider-nav",appendDots:".navig-dots1",customPaging:function(a,b){return'<div class="navig-dot1"></div>'}});$(".slider-nav").slick({slidesToShow:3,slidesToScroll:1,dots:!1,lazyLoad:"ondemand",
-arrows:!1,asNavFor:".slider-for",centerMode:!0,focusOnSelect:!0,responsive:[{breakpoint:480,settings:{settings:"unslick"}}]})})();(function(){function a(){c.forEach(function(a){a.classList.remove("is-active")});this.classList.add("is-active");d=this.getAttribute("data-tab-name");b(d)}function b(a){e.forEach(function(b){b.classList.contains(a)?b.classList.add("is-active"):b.classList.remove("is-active")})}var c=document.querySelectorAll(".tabs-nav__item"),e=document.querySelectorAll(".tab"),d;c.forEach(function(b){b.addEventListener("click",
-a)})})();(function(){$(".tabs-nav").slick({mobileFirst:!0,arrows:!1,dots:!0,appendDots:".navig-dots2",customPaging:function(a,b){return'<div class="navig-dot2"></div>'},responsive:[{breakpoint:801,settings:"unslick",slidesToShow:3,arrows:!1},{breakpoint:400,slidesToShow:1,arrows:!1}]})})();(function(){$(".mask").mask("+7 (999) 999-99-99");$("body").on("click",".popup-view",function(){$(".callme-popup .form-hide .popup-form__title").text($(this).data("title"));$(".callme-popup .form-hide .popup-form__desc").text($(this).data("desc"));
-"\u0414\u0430"==$(this).data("file")?$(".callme-popup .form-hide .popup-form__file").show():$(".callme-popup .form-hide .popup-form__file").hide();$(".callme-popup .form-metka").val($(this).data("metka"));$(".callme-popup .form-button").text($(this).data("button"));$(".callme-popup").fadeIn(300);$(".popup-form").animate({opacity:1},300,"linear");$("btn-"+$(this).data("id"));return!1});$(".callme-popup").on("click",".popup-close, .popup-close--2",function(){$(".popup-form").animate({opacity:0},300,
-"linear");setTimeout(function(){$(".callme-popup").fadeOut(300)},200)});$(".form-js").each(function(){var a="#"+$(this).attr("id"),b=$(this).attr("id");$(this).on("click",".form-button",function(){if(""==$(a+" .validate").val())return $(a+" .validate").css({"border-color":"#de2827"}),setTimeout(function(){$(".validate").removeAttr("style")},500),!1;if($(a+" .confidencial__checked input").prop("checked"))$(a).closest(".form-hide").hide(),$(a).closest(".form-hide").siblings(".form-ok").fadeIn(100),
-setTimeout(function(){var a=new Event("submit");document.getElementById(b).dispatchEvent(a)},500),document.getElementById(b).addEventListener("submit",function(a){var b=new XMLHttpRequest;a.preventDefault();b.open("POST","/js/contacts.php",!0);b.onerror=function(){console.log("\u0414\u0430\u043d\u043d\u044b\u0435 \u043d\u0435 \u0431\u044b\u043b\u0438 \u043f\u0435\u0440\u0435\u0434\u0430\u043d\u044b")};b.send(new FormData(this))},!1),console.log("\u041e\u0442\u043f\u0440\u0430\u0432\u0438\u043b\u0438");
-else return $(a+" .confidencial__checked").css("color","#de2827"),setTimeout(function(){$(".confidencial__checked").removeAttr("style")},500),!1})})})();(function(){$(".image-modal").each(function(){$(this).magnificPopup({type:"inline",fixedContentPos:!0,removalDelay:100,closeBtnInside:!0,preloader:!0,mainClass:"mfp-fade"})});$(document).on("click",".popup-modal-dismiss",function(a){a.preventDefault();$.magnificPopup.close()});new jPlayerPlaylist({jPlayer:"#jquery_jplayer_2",cssSelectorAncestor:"#jp_container_2"},
-[{title:"\u0427\u0435\u0441\u043d\u044b\u0439 \u0430\u0443\u0434\u0438\u043e-\u043e\u0442\u0437\u044b\u0432 #1",mp3:"audio/1.mp3"},{title:"\u0427\u0435\u0441\u043d\u044b\u0439 \u0430\u0443\u0434\u0438\u043e-\u043e\u0442\u0437\u044b\u0432 #2",mp3:"audio/2.mp3"},{title:"\u0427\u0435\u0441\u043d\u044b\u0439 \u0430\u0443\u0434\u0438\u043e-\u043e\u0442\u0437\u044b\u0432 #3",mp3:"audio/3.mp3"},{title:"\u0427\u0435\u0441\u043d\u044b\u0439 \u0430\u0443\u0434\u0438\u043e-\u043e\u0442\u0437\u044b\u0432 #4",
-mp3:"audio/4.mp3"}],{swfPath:"jplayer",supplied:"oga, mp3",wmode:"window",useStateClassSkin:!0,autoBlur:!1,smoothPlayBar:!0,keyEnabled:!0})})()});
+$(document).ready(function () {
+//скролл
+let scrollLinks = function() {
+  $('.nav-link').click(function (){
+  var elementClick = $(this).attr("href");
+  var destination = $(elementClick).offset().top;
+  jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+  return false;
+  });
+}
+
+
+let numbers = function() {
+var section = document.querySelector('.numbers');
+var hasEntered = false;
+
+window.addEventListener('scroll', (e) => {
+  var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
+
+  if (shouldAnimate && !hasEntered) {
+    hasEntered = true;
+    
+    $('.value').each(function () {
+      $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+      });
+    });
+
+  }
+});
+}
+
+
+//wow
+let wow = function() {
+  new WOW().init();
+}
+
+//slider
+let jsSlider = function() {
+  $('.js-rev-slider').slick({
+  centerMode: true,
+  variableWidth: true,
+  slidesToShow: 1,
+  prevArrow: '.navigation--prev',
+  nextArrow: '.navigation--next',
+  autoplay: true,
+});
+}
+
+//popupGallery
+let popupGallery = function() {
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+      }
+    }
+  });
+}
+
+//scrollMagig
+
+let scrollM = function() {
+  let t1 = new TimelineMax({onUpdate:updatePercentage});
+  const controller = new ScrollMagic.Controller();
+
+
+  t1.from(".line", .5, {height: 0});
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".s-who",
+    triggerHook: "onLeave",
+    duration: "100%"
+  })
+  .setPin(".s-who")
+  .setTween(t1)
+    .addTo(controller);
+
+  function updatePercentage() {
+    t1.progress();
+    console.log(t1.progress());
+  }
+};
+let scrollM1 = function() {
+  let t1 = new TimelineMax({onUpdate:updatePercentage});
+  const controller = new ScrollMagic.Controller();
+
+  t1.from(".line2", .5, {height: 0});
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".s-kriptoM",
+    triggerHook: "onLeave",
+    duration: "100%"
+  })
+  .setPin(".s-kriptoM")
+  .setTween(t1)
+    .addTo(controller);
+
+  function updatePercentage() {
+    t1.progress();
+    console.log(t1.progress());
+  }
+};
+
+numbers();
+jsSlider();
+popupGallery();
+scrollLinks();
+
+if ($(window).width() > 1025) {
+  scrollM1();
+  scrollM();
+  wow();
+}
+});
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline="January 01 2018 00:00:00 GMT+0300"; //for Ukraine
+var deadline = new Date(Date.parse(new Date()) + 2 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
+
