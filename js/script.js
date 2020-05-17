2646cbadac8206f101b954aa157fe7a1e9f93b86
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-05-17 18:37:55 +0300

    brend

--- js/script.js
+++ js/script.js
@@ -1,186 +1,97 @@
-function hidePreloader() {
-      let preloader = document.getElementById('preloader')
-      preloader.style.opacity = "0"; //fade out transition
-      setTimeout(function() {
-        preloader.style.display = "none";
-      }, 2000);
-    }
-
-
 $(document).ready(function () {
-
-
 //скролл
-let scrollLinks = function() {
-  $('.nav-link').click(function (){
-  var elementClick = $(this).attr("href");
-  var destination = $(elementClick).offset().top;
-  jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
-  return false;
-  });
-}
-
-
-let numbers = function() {
-var section = document.querySelector('.numbers');
-var hasEntered = false;
-
-window.addEventListener('scroll', (e) => {
-  var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
-
-  if (shouldAnimate && !hasEntered) {
-    hasEntered = true;
-    
-    $('.value').each(function () {
-      $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-      }, {
-        duration: 5000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-      });
-    });
-
-  }
+$('.nav-link').click(function (){
+var elementClick = $(this).attr("href");
+var destination = $(elementClick).offset().top;
+jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+return false;
 });
+
+
+let waterWill = function() {
+	var carousel = $("#waterwheel-carousel").waterwheelCarousel({
+	    flankingItems: 3,
+	    separation:  250,
+	  });
+
+	  $('#prev').bind('click', function () {
+	    carousel.prev();
+	    return false
+	  });
+
+	  $('#next').bind('click', function () {
+	    carousel.next();
+	    return false;
+	  });
 }
 
 
-//wow
-let wow = function() {
-  new WOW().init();
-}
-
-//slider
-let jsSlider = function() {
-  $('.js-rev-slider').slick({
-  centerMode: true,
-  variableWidth: true,
-  slidesToShow: 1,
-  prevArrow: '.navigation--prev',
-  nextArrow: '.navigation--next',
-  autoplay: true,
-});
-}
-
-//popupGallery
-let popupGallery = function() {
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+//rev slider
+let revSlider = function() {
+	$('.js-rev-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '.prev',
+    nextArrow: '.next',
+    focusOnSelect: true,
+    centerMode: true,
+    centerPadding: 0,
+     responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
     },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     }
-  });
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });
+
 }
+  
+//youtube bg 
 
-//scrollMagig
-
-let scrollM = function() {
-  let t1 = new TimelineMax({onUpdate:updatePercentage});
-  const controller = new ScrollMagic.Controller();
-
-
-  t1.from(".line", .5, {height: 0});
-  const scene = new ScrollMagic.Scene({
-    triggerElement: ".s-who",
-    triggerHook: "onLeave",
-    duration: "100%"
-  })
-  .setPin(".s-who")
-  .setTween(t1)
-    .addTo(controller);
-
-  function updatePercentage() {
-    t1.progress();
-    console.log(t1.progress());
-  }
-};
-let scrollM1 = function() {
-  let t1 = new TimelineMax({onUpdate:updatePercentage});
-  const controller = new ScrollMagic.Controller();
-
-  t1.from(".line2", .5, {height: 0});
-  const scene = new ScrollMagic.Scene({
-    triggerElement: ".s-kriptoM",
-    triggerHook: "onLeave",
-    duration: "100%"
-  })
-  .setPin(".s-kriptoM")
-  .setTween(t1)
-    .addTo(controller);
-
-  function updatePercentage() {
-    t1.progress();
-    console.log(t1.progress());
-  }
-};
-
-
-numbers();
-jsSlider();
-popupGallery();
-scrollLinks();
-
-if ($(window).width() > 1025) {
-  scrollM1();
-  scrollM();
-  wow();
-}
+$('[data-youtube]').youtube_background({
+	'play-button': true,
+	'autoplay': false
 });
 
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+//sandwich
+let sanwichToggle = function() {
+        $('.sandwich').click(function(){
+           $('.sandwich-line').toggleClass('sandwich--active');
+           $('.nav-wrapper').fadeToggle('slow'); 
+        });
+         $('.nav-link').click(function(){
+           $('.sandwich-line').toggleClass('sandwich--active');
+           $('.nav-wrapper').fadeToggle('slow'); 
+        });
+    };
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
+ if( window.innerWidth <= 800 ){
+      sanwichToggle();
+ } else {
+      //не выполнять
+ } 
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+waterWill();
+revSlider();
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
+});
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
 
-var deadline="January 01 2018 00:00:00 GMT+0300"; //for Ukraine
-var deadline = new Date(Date.parse(new Date()) + 2 * 24 * 60 * 60 * 1000); // for endless timer
-initializeClock('countdown', deadline);
+/* jquery.youtube-background v1.0.7 | Nikola Stamatovic <@stamat> | MIT */
+
+
 
