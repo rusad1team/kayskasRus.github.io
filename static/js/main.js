b87a71ea0ab23b6af2756aa941bc958ae7c52eea
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-09-18 21:50:24 +0300

    mass

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
 //скролл
-$('.btnScroll').click(function (){
+$('.banner-bottom__link').on('click', function (){
 var elementClick = $(this).attr("href");
 var destination = $(elementClick).offset().top;
 jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
 return false;
 });
-
-$('.js-banner-slider').slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    fade: true,
-    autoplaySpeed: 7000,
-  });
-
+$('.search').on('click', function (){
+    $('.form-search__wrapper').addClass('form-search--active');
+});
+$('.form-search__close').on('click', function (){
+    $('.form-search__wrapper').removeClass('form-search--active');
+});
+$(window).on('scroll', function(){
+    if ($(window).scrollTop() >= 1) {
+        $('.header').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('.header').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
+$('.foooter-more').on('click', function (){
+        $('.footer-block--ul1 li:not(.footer-link-active)').slideToggle();
+});
 
 });
 
