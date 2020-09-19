525d1d131cb1bfab9bdf48882c73afd1a6c6ed9f
kayskasRus <48016815+kayskasRus@users.noreply.github.com>
2020-09-19 18:55:47 +0300

    mangal

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,41 @@
 $(document).ready(function () {
 //скролл
-$('.banner-bottom__link').on('click', function (){
+$('.btnScroll').click(function (){
 var elementClick = $(this).attr("href");
 var destination = $(elementClick).offset().top;
 jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
 return false;
 });
 $('.search').on('click', function (){
-    $('.form-search__wrapper').addClass('form-search--active');
+  $('.form-search__wrapper').addClass('form-search--active');
+  $(this).addClass('search-toggle');
 });
 $('.form-search__close').on('click', function (){
-    $('.form-search__wrapper').removeClass('form-search--active');
+  $('.form-search__wrapper').removeClass('form-search--active');
+  $('.search').removeClass('search-toggle');
 });
-$(window).on('scroll', function(){
-    if ($(window).scrollTop() >= 1) {
-        $('.header').addClass('fixed-header');
-        $('nav div').addClass('visible-title');
-    }
-    else {
-        $('.header').removeClass('fixed-header');
-        $('nav div').removeClass('visible-title');
-    }
-});
-$('.foooter-more').on('click', function (){
-        $('.footer-block--ul1 li:not(.footer-link-active)').slideToggle();
+$('.js-banner-slider').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 7000,
+  });
+let sanwichToggle = function() {
+    $('.sandwich').on('click', function(){
+       $('.sandwich-line').toggleClass('sandwich--active');
+       $('.nav-wrapper').fadeToggle('slow'); 
+    });
+};
+
+sanwichToggle();
+
+$('.popup-modal').magnificPopup({
+  type: 'inline',
+  preloader: false,
+  focus: '#name'
 });
 
 });
